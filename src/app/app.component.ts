b11a81c7b1da241a5c4c56ec7cdd4f7512b1342b
Elea641 <(mailto:eleavoliotis64@gmail.com)>
2023-01-03 15:28:50 +0100

    Fin première partie appli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap';
 
 @Component({
   selector: 'app-root',
@@ -7,58 +6,7 @@ import { FaceSnap } from './models/face-snap';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  myDou!: FaceSnap;
-  myDoudou!: FaceSnap;
-  myDoudidou!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
-  mySnap!: FaceSnap;
 
-  ngOnInit(){
-    this.mySnap = {
-      title: 'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'Paris'
-    };
-    this.myOtherSnap = {
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'Tokyo'
-    };
-    this.myLastSnap = {
-      title: 'Un bon repas',
-      description: 'Mmmh que c\'est bon !',
-      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    };
-    this.myDou = {
-      title: 'Doudou',
-      description: 'Tout tout doux',
-      imageUrl: 'https://media.licdn.com/dms/image/C4D03AQH1H3PC1rOW_A/profile-displayphoto-shrink_800_800/0/1620434605775?e=1678320000&v=beta&t=P4whu9A36khuAtu8XtXal0nBon9IXIpAb6ATv7Lf6kU',
-      createdDate: new Date(),
-      snaps: 0
-    };
-    this.myDoudou = {
-      title: 'Doudoudoudou',
-      description: 'Tout tout doudoux',
-      imageUrl: 'https://media.licdn.com/dms/image/C4D03AQH1H3PC1rOW_A/profile-displayphoto-shrink_800_800/0/1620434605775?e=1678320000&v=beta&t=P4whu9A36khuAtu8XtXal0nBon9IXIpAb6ATv7Lf6kU',
-      createdDate: new Date(),
-      snaps: 0
-    };
-    this.myDoudidou = {
-      title: 'Doudidou',
-      description: 'Tout tout doudidoux',
-      imageUrl: 'https://media.licdn.com/dms/image/C4D03AQH1H3PC1rOW_A/profile-displayphoto-shrink_800_800/0/1620434605775?e=1678320000&v=beta&t=P4whu9A36khuAtu8XtXal0nBon9IXIpAb6ATv7Lf6kU',
-      createdDate: new Date(),
-      snaps: 0
-    }
-  }
+  ngOnInit(){}
 
 }
--- src/app/single-face-snap/single-face-snap.component.ts
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -1,22 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FaceSnap } from '../models/face-snap';
 import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
-  selector: 'app-face-snap',
-  templateUrl: './face-snap.component.html',
-  styleUrls: ['./face-snap.component.scss']
+  selector: 'app-single-face-snap',
+  templateUrl: './single-face-snap.component.html',
+  styleUrls: ['./single-face-snap.component.scss']
 })
-export class FaceSnapComponent implements OnInit{
-  @Input() faceSnap!: FaceSnap;
+export class SingleFaceSnapComponent {
+  faceSnap!: FaceSnap;
   buttonText!: string;
 
   constructor(private faceSnapsService: FaceSnapsService,
-              private router: Router) {}
+              private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.buttonText = 'Oh Snap!';
+    const snapId = +this.route.snapshot.params['id'];
+    this.faceSnap = this.faceSnapsService.getFaceSnapById(snapId);
   }
 
   onSnap() {
@@ -27,9 +29,5 @@ export class FaceSnapComponent implements OnInit{
       this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
       this.buttonText = 'Oh Snap!';
       }
-    }
-
-    onViewFaceSnap() {
-      this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
     }
 }
